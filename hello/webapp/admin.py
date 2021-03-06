b5b668c7a93f6e6cc0a849320952192693da5052
Aiganysh <aiganyshx@gmail.com>
2021-03-06 12:32:18 +0600

    add and registered model in admin.py

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Guestbook
+
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'author', 'email', 'text', 'time_of_creation', 'time_of_update']
+    list_filter = ['author', 'status', 'text']
+    search_fields = ['status', 'describe', 'description']
+    fields = ['status', 'author', 'email', 'text', 'time_of_creation', 'time_of_update']
+    readonly_fields = ['id', 'time_of_creation', 'time_of_update']
+
+
+admin.site.register(Guestbook, BookAdmin)
