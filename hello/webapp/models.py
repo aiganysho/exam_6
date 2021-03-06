6b986daefc467479d5f17c6f945fb297235261cd
Aiganysh <aiganyshx@gmail.com>
2021-03-06 12:29:03 +0600

    Pass the data in the model

--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+status_choices = [("active", "Активно"), ("blocked", "Заблокировано")]
 # Create your models here.
+
+class Guestbook(models.Model):
+    author = models.CharField(max_length=150, null=False, blank=False,  verbose_name='Автор')
+    email = models.EmailField(max_length=150, null=False, blank=False, verbose_name='Почта автора')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст записи')
+    time_of_creation = models.DateTimeField(auto_now_add=True)
+    time_of_update = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=150, null=False, blank=False, choices=status_choices, default="active")
+
+    class Meta:
+        db_table = 'Guestbook'
+        verbose_name = 'Гостевая книга'
+        verbose_name_plural = 'Гостевая книги'
+
+    def __str__(self):
+        return f'{self.id}. {self.author}'
